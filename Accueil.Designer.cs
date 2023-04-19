9b900d5effb12a0186b552d26b10479050762515
LaurentAFPA <laurentcampan@outlook.fr>
2023-04-19 15:47:36 +0200

    Ajoutez des fichiers projet.

---
+++ Accueil.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Morpion
+{
+    partial class frmAccueil
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnJouer = new System.Windows.Forms.Button();
+            this.btnInscription = new System.Windows.Forms.Button();
+            this.lblTitre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnJouer
+            // 
+            this.btnJouer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJouer.Location = new System.Drawing.Point(258, 314);
+            this.btnJouer.Name = "btnJouer";
+            this.btnJouer.Size = new System.Drawing.Size(145, 60);
+            this.btnJouer.TabIndex = 0;
+            this.btnJouer.Text = "Jouer";
+            this.btnJouer.UseVisualStyleBackColor = true;
+            // 
+            // btnInscription
+            // 
+            this.btnInscription.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInscription.Location = new System.Drawing.Point(636, 314);
+            this.btnInscription.Name = "btnInscription";
+            this.btnInscription.Size = new System.Drawing.Size(164, 60);
+            this.btnInscription.TabIndex = 1;
+            this.btnInscription.Text = "S\'inscrire";
+            this.btnInscription.UseVisualStyleBackColor = true;
+            // 
+            // lblTitre
+            // 
+            this.lblTitre.AutoSize = true;
+            this.lblTitre.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitre.Location = new System.Drawing.Point(411, 95);
+            this.lblTitre.Name = "lblTitre";
+            this.lblTitre.Size = new System.Drawing.Size(189, 39);
+            this.lblTitre.TabIndex = 2;
+            this.lblTitre.Text = "Bienvenue";
+            // 
+            // frmAccueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.lblTitre);
+            this.Controls.Add(this.btnInscription);
+            this.Controls.Add(this.btnJouer);
+            this.Name = "frmAccueil";
+            this.Text = "Accueil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJouer;
+        private System.Windows.Forms.Button btnInscription;
+        private System.Windows.Forms.Label lblTitre;
+    }
+}
+
